2d6d55e6cc6f0610e10c53b1035c38d9a344b414
Tapon Paul <tapon788@gmail.com>
2024-07-09 21:00:19 +0600

    updated call type detection logic and initial logic for VoNR call

--- packetparsing/case_1a_NR_dedicated_bearer.py
+++ packetparsing/case_1a_NR_dedicated_bearer.py
@@ -12,25 +12,20 @@ for files in os.listdir('out'):
 
 
 def chk4gOr5gCall(filename,containString,containString2,field):
-
     filelocation = 'out\\'+filename
-    cap = pyshark.FileCapture(f'{filelocation}', display_filter=f'_ws.col.info contains \"{containString}\"')
+    cap = pyshark.FileCapture(f'{filelocation}', display_filter=f'_ws.col.info contains \"{containString}\" or _ws.col.info contains \"{containString2}\"')
     cap.load_packets()
-    cap2 = pyshark.FileCapture(f'{filelocation}', display_filter=f'_ws.col.info contains \"{containString2}\"')
-    cap2.load_packets()
-
+    invite_arrival_time = cap[0].sniff_time
     typeOfCall = ''
     if cap:
-        pkt = cap[0]
-        invite_arrival_time = pkt.sniff_time
-
-        try:
-            typeOfCall = pkt.sip._all_fields['sip.P-Access-Network-Info.access-type']
-        except KeyError:
-            typeOfCall = cap2[0].sip._all_fields['sip.P-Access-Network-Info.access-type']
-        except:
+        for pkt in cap:
+            try:
+                typeOfCall = pkt.sip._all_fields['sip.P-Access-Network-Info.access-type']
+                break
+            except KeyError:
+                continue
+        if not typeOfCall:
             typeOfCall = 'Unknown'
-
         return {'callType':typeOfCall,'inviteTime':invite_arrival_time}
     else:
         return None
@@ -44,12 +39,9 @@ def chkPDUSesResModReqAnd5QI(filename, containString, field):
         pkt = cap[0]
         resp = chkPDUSesResModResponse(filename,'PDUSessionResourceModifyResponse')
         try:
-            #return {'PDUSesResModReq':'PDUSessionResourceModifyRequest','5qi':pkt.ngap._all_fields['ngap.fiveQI']}
             return {'PDUSesResModReq': 'PDUSessionResourceModifyRequest', 'msg': resp['msg'], 'comment':resp['comment']}
         except:
-            #return {'PDUSesResModReq':'PDUSessionResourceModifyRequest','5qi':99}
             return {'PDUSesResModReq': 'PDUSessionResourceModifyRequest', 'resp': 'unknown'}
-
     else:
         return None #{'PDUSesResModReq':'N/A','5qi':'N/A'}
 
@@ -67,14 +59,12 @@ def chkPDUSesResModResponse(filename, containString):
     filt = f'_ws.col.info contains \"{containString}\"'
     cap = pyshark.FileCapture(f'{filelocation}', display_filter=filt)
     cap.load_packets()
-
-
     pkt = cap[0]
-    msg = pkt.ngap._all_fields['ngap.radioNetwork']
-
+    try:
+        msg = pkt.ngap._all_fields['ngap.radioNetwork']
+    except KeyError:
+        msg = pkt.ngap._all_fields['ngap.qosFlowIdentifier']
     comment = ''
-    #radioNetwork: xn-handover-triggered (33)
-    #radioNetwork: ims - voice - eps - fallback - or -rat - fallback - triggered(36)
     if msg=='33':
         msg = 'radio network: xn-handover-triggered(33)'
         comment = 'gNB responded with xn-handover-triggered'
@@ -85,6 +75,9 @@ def chkPDUSesResModResponse(filename, containString):
             comment = 'TAU msg after EPS FB, Need to check further'
         else:
             comment = 'No TAU msg after EPS FB'
+    elif msg=='1':
+        msg = 'qosFlowIdentifier=1'
+        comment = "VoNR call. Need to check further"
     else:
         msg = 'unknown'
         comment = 'unknown'
@@ -101,7 +94,6 @@ cell_format_header = wb.add_format(
 cell_format_header.set_font_size(10)
 cell_format_data = wb.add_format()
 cell_format_data.set_font_size(8)
-
 colheader = ['sourcePcap',
              'problematicCallId',
              'inviteTime',
@@ -110,27 +102,23 @@ colheader = ['sourcePcap',
              'response',
              'comment',
              ]
-
-
-
 row = 0
 for header in colheader:
     ws.write(row, colheader.index(header), header, cell_format_header)
 
 for filename in pcapfiles:
-
     print(f'Working with {filename}')
     row += 1
     callTypeInfo = chk4gOr5gCall(filename,'Request: INVITE','Status: 183 Session Progress',"sip.P-Access-Network-Info.access-type")
     pduInfo = chkPDUSesResModReqAnd5QI(filename, 'PDUSessionResourceModifyRequest', 'ngap.fiveQI')
-
+    if not pduInfo:
+        pduInfo = {'PDUSesResModReq':'No PDUSessionResourceModifyRequest','msg':'Call failed in early stage','comment':'Need to check further'}
     source_file = '_'.join(filename.split('.')[0].split('_')[:-1])
     ws.write(row, 0, source_file, cell_format_data)
     ws.write(row, 1, filename.split('.')[0].split('_')[-1], cell_format_data)
     ws.write(row, 2, str(callTypeInfo['inviteTime']), cell_format_data)
     ws.write(row, 3, callTypeInfo['callType'], cell_format_data)
-
-    if callTypeInfo['callType'].find('3GPP-NR-')>=0:
+    if callTypeInfo['callType'].find('3GPP-NR-')>=0 or callTypeInfo['callType']=='Unknown':
         if pduInfo:
             ws.write(row,4,pduInfo['PDUSesResModReq'],cell_format_data)
             ws.write(row,5,pduInfo['msg'],cell_format_data)
@@ -139,7 +127,6 @@ for filename in pcapfiles:
             ws.write(row,4,'N/A',cell_format_data)
             ws.write(row,5,'N/A',cell_format_data)
             ws.write(row,6,'No dedicated bearer from core',cell_format_data)
-
     else:
         ws.write(row, 4, 'Not analysed', cell_format_data)
         ws.write(row, 5, 'Not analysed', cell_format_data)
